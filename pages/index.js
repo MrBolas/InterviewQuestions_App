31b3cb484a309def1bc3f23c5e3cfe6d19d37a89
João Soares <joao.soares@truphone.com>
2022-06-19 16:02:57 +0100

    Increase widt of question block

--- pages/index.js
+++ pages/index.js
@@ -34,14 +34,14 @@ export default function Home() {
 
   return (
   <div className="flex justify-center">
-    <div className="block p-6 rounded-lg shadow-lg bg-white max-w-sm">
+    <div className="block p-6 rounded-lg shadow-lg bg-white max-w-lg">
+      <button type="button" className=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out" onClick={getNewQuestion}>New question</button>
+      <button type="button" className=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out" onClick={toggleAnswer}>{`${showAnswer ? "Hide" : "Show"} answer`}</button>
+      <Link href={randomQuestion['source']} rel="noreferrer noopener" passHref><a type="button" className=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out" target="_blank"> Source </a></Link>
       <h5 className="text-gray-900 text-xl leading-tight font-medium mb-2">{randomQuestion['question']}</h5>
       <p className="text-gray-700 text-base mb-4">
         {showAnswer && <p className="text-grey-700 text-base">{randomQuestion['answer']}</p>}
       </p>
-      <button type="button" className=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out" onClick={getNewQuestion}>Get new question</button>
-      <button type="button" className=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out" onClick={toggleAnswer}>{`${showAnswer ? "Hide" : "Show"} answer`}</button>
-      <Link href={randomQuestion['source']} rel="noreferrer noopener" passHref><a type="button" className=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out" target="_blank"> Source </a></Link>
     </div>
   </div>
   )
