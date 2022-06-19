480b9251df347330d6d73ecd63cef14145247715
João Soares <joao.soares@truphone.com>
2022-06-19 16:52:13 +0100

    Added emojis to dificulty

--- pages/index.js
+++ pages/index.js
@@ -15,6 +15,14 @@ export default function Home() {
     }
   )
 
+  const seniority = {
+    "entry": "👨‍💻",
+    "junior": "👨‍💻👨‍💻",
+    "mid": "👨‍💻👨‍💻👨‍💻",
+    "senior": "👨‍💻👨‍💻👨‍💻👨‍💻",
+    "expert": "👨‍💻👨‍💻👨‍💻👨‍💻👨‍💻"
+  }
+
   const openQuestionSource = useCallback(() => {
     window.open(randomQuestion['source'], "_blank")
   },[])
@@ -36,7 +44,7 @@ export default function Home() {
   <div className="flex justify-center">
     <div className="block p-6 rounded-lg shadow-lg bg-white max-w-lg">
     <p>
-        <h5 className="text-gray-900 text-xl leading-tight font-medium mb-2">{randomQuestion['level']} {randomQuestion['category']}</h5>
+        <h5 className="text-gray-900 text-xl leading-tight font-medium mb-2">{seniority[randomQuestion['level']]} {randomQuestion['category']}</h5>
       </p>
         <button type="button" className=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out" onClick={getNewQuestion}>New question</button>
         <button type="button" className=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out" onClick={toggleAnswer}>{`${showAnswer ? "Hide" : "Show"} answer`}</button>
