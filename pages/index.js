b8b5f8fc60753b9b287559a64b4ca0c9f3347164
João Soares <joao.soares@truphone.com>
2022-06-19 16:39:31 +0100

    Added category and level to the interface

--- pages/index.js
+++ pages/index.js
@@ -35,6 +35,9 @@ export default function Home() {
   return (
   <div className="flex justify-center">
     <div className="block p-6 rounded-lg shadow-lg bg-white max-w-lg">
+    <p>
+        <h5 className="text-gray-900 text-xl leading-tight font-medium mb-2">{randomQuestion['level']} {randomQuestion['category']}</h5>
+      </p>
         <button type="button" className=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out" onClick={getNewQuestion}>New question</button>
         <button type="button" className=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out" onClick={toggleAnswer}>{`${showAnswer ? "Hide" : "Show"} answer`}</button>
         <Link href={randomQuestion['source']} rel="noreferrer noopener" passHref><a type="button" className=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out" target="_blank"> Source </a></Link>
